7203ca878db146c088d830e2b5c6b85a9420be20
ldzzz <eldinabdic47@gmail.com>
2024-07-10 22:29:03 +0200

    CHG: add config loading and logging printing

--- cmd/kacky-gamserver-backend/main.go
+++ cmd/kacky-gamserver-backend/main.go
@@ -1,5 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log/slog"
 	"os"
+
+	"github.com/ldzzz/kacky-gameserver-backend/config"
 )
+
+func main() {
+	// load config environment variables
+	var cfg *config.Config
+	var err error
+	if cfg, err = config.ReadConfig(); err != nil {
+		fmt.Printf("Couldn't read configuration environment variables: %s\n", err)
+		os.Exit(1)
+	}
+
+	// initialize Logging
+	config.InitLogging(cfg.Debug)
+	slog.Info("Initialized configuration & logging")
+	//slog.Debug("Environment variables loaded:", cfg)
+
+	// TODO: open connection to db
+	// TODO: open connection to NATS
+	// TODO: setup callbacks for all NATS channels
+	// TODO: define error handling ??
+	// TODO: write unit tests
+	// TODO: write integration tests
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ldzzz/kacky-gameserver-backend
 
 go 1.22.5
+
+require github.com/sethvargo/go-envconfig v1.1.0
