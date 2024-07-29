f27a1cbdf95e1ac6a0c0fb0209b67dbbdb83ac98
ldzzz <eldinabdic47@gmail.com>
2024-07-29 20:02:47 +0200

    ADD: start writing queries

--- db/query.sql
+++ db/query.sql
@@ -1,14 +1,15 @@
--- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = ? LIMIT 1;
+-- name: GetPlayer :one
+SELECT * FROM tm_players
+WHERE login = $1;
 
--- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
-
--- name: CreateAuthor :execresult
-INSERT INTO authors (
-  name, bio
+-- name: CreatePlayer :exec
+INSERT INTO tm_players (
+  login, nickname, zone, game_type
 ) VALUES (
-  ?, ?
+  $1, $2, $3, $4
 );
+
+-- name: GetPlayerFinishes :many
+SELECT finishes.* FROM finishes
+JOIN tm_players ON finishes.player_id = tm_players.id
+WHERE tm_players.login = $1 AND tm_players.game_type = $2;
